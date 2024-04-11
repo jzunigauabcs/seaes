9fb20d6a43a01db8c45b29bb7890931226fbe89a
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2024-04-11 01:39:24 -0700

    fix(page title)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home'
 import Indicadores from './pages/indicadores/Indicadores';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        document.title = "SEAES"
+    }, [])
   return (
     <>
       <Routes>
